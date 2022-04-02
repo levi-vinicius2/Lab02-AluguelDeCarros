576f1d23344f40ad044fb3d3499e4fa4eac113c8
Dalila <dalilamv99@outlook.com>
2022-04-02 11:45:37 -0300

    add spring initial config

---
+++ Implementacao/alugueldecarros/src/main/java/com/alugueldecarros/alugueldecarros/AlugueldecarrosApplication.java
@@ -0,0 +1,13 @@
+package com.alugueldecarros.alugueldecarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugueldecarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugueldecarrosApplication.class, args);
+	}
+
+}
---
+++ Implementacao/alugueldecarros/src/test/java/com/alugueldecarros/alugueldecarros/AlugueldecarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alugueldecarros.alugueldecarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugueldecarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
