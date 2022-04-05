fb36ac0be07cafd72e95f7a23d0389181609c05b
Dalila <dalilamv99@outlook.com>
2022-04-04 22:41:01 -0300

    criada a inclusão de clientes

--- Implementacao/alugueldecarros/src/main/java/com/alugueldecarros/alugueldecarros/cliente/controller/ClienteController.java
+++ Implementacao/alugueldecarros/src/main/java/com/alugueldecarros/alugueldecarros/cliente/controller/ClienteController.java
@@ -1,9 +1,12 @@
 package com.alugueldecarros.alugueldecarros.cliente.controller;
 
+import com.alugueldecarros.alugueldecarros.cliente.model.Cliente;
 import com.alugueldecarros.alugueldecarros.cliente.model.ClienteRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ClienteController {
@@ -18,5 +21,16 @@ public class ClienteController {
     public String clientes(Model model) {
         model.addAttribute("listaClientes",clienteRepo.findAll());
         return "clientes/index";
+    }
+
+    @GetMapping("/clientes/nova")
+    public String novoCliente(@ModelAttribute("cliente") Cliente cliente) {
+        return "clientes/form";
+    }
+
+    @PostMapping("/clientes/salvar")
+    public String salvarCliente(@ModelAttribute("cliente") Cliente cliente) {
+        clienteRepo.save(cliente);
+        return "redirect:/clientes";
     }
 }
